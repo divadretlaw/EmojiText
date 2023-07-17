b84c5645eb5c16f6053232099327d20d7d440f82
David Walter <hello@davidwalter.at>
2023-07-16 23:29:13 -0300

    Use Testplan

--- Test/Tests/EmojiTextTests.swift
+++ Test/Tests/EmojiTextTests.swift
@@ -10,7 +10,7 @@ import EmojiText
 import SnapshotTesting
 import SwiftUI
 
-final class TestTests: XCTestCase {
+final class EmojiTextTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         isRecording = false
