ef6fdb58f9c38ea4f0f02923b26e433216629363
David Walter <hello@davidwalter.at>
2023-12-07 16:24:48 +0100

    Add Package.swift for Swift 5.7 & 5.8

---
+++ Package@swift-5.8.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "EmojiText",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "EmojiText",
+            targets: ["EmojiText"]
+        )
+    ],
+    dependencies: [
+         .package(url: "https://github.com/kean/Nuke", from: "12.0.0")
+    ],
+    targets: [
+        .target(
+            name: "EmojiText",
+            dependencies: [
+                .product(name: "Nuke", package: "Nuke")
+            ]
+        )
+    ]
+)
